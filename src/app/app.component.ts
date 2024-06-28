4540f78636e2899c933d7f784c760366163a63fe
Illya <romanenkoisnot@gmail.com>
2024-06-28 16:12:27 +0300

    fix: styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./widgets/shared/header/header.component";
+import { FooterComponent } from "./widgets/shared/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
+    selector: 'app-root',
+    standalone: true,
+    template: `
+    <app-header></app-header>
     <router-outlet />
+    <app-footer></app-footer>
   `,
-  styles: [],
+    styles: [],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'ctrlShop';
