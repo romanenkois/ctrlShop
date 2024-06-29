5f837e72153e8515367b56073f40dbb82ab36d78
Illya <romanenkoisnot@gmail.com>
2024-06-29 23:19:30 +0300

    feat: cart widget for header

--- src/app/widgets/shared/header/header.component.ts
+++ src/app/widgets/shared/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
+import { CartComponent } from "./ui/cart/cart.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [CartComponent]
 })
 export class HeaderComponent {
   redirectTo(url: string) {
     window.location.href = url;
   }
-  clothesMenu = false;
+
+  clothesMenuVisibility = false;
+  cartVisibility = false;
 
   toggleClothesMenu() {
-    this.clothesMenu = !this.clothesMenu;
+    this.clothesMenuVisibility = !this.clothesMenuVisibility;
+  }
+  togleCart() {
+    this.cartVisibility = !this.cartVisibility;
   }
 }
