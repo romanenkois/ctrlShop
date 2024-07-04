aeaa30607658937a8c6c396128075d19ba19bece
Illya <romanenkoisnot@gmail.com>
2024-07-05 02:11:58 +0300

    feat: clothes page update

--- src/app/pages/clothes/clothes.component.ts
+++ src/app/pages/clothes/clothes.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, WritableSignal, effect, inject, signal } from '@angular/core';
+import { ClothesFilterComponent } from "../../widgets/clothes-page/clothes-filter/clothes-filter.component";
+import { ClothesFeedComponent } from "../../widgets/clothes-page/clothes-feed/clothes-feed.component";
+import { ClothesSortingComponent } from "../../widgets/clothes-page/clothes-sorting/clothes-sorting.component";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-clothes',
-  standalone: true,
-  imports: [],
-  templateUrl: './clothes.component.html',
-  styleUrl: './clothes.component.scss'
+    selector: 'app-clothes',
+    standalone: true,
+    templateUrl: './clothes.component.html',
+    styleUrl: './clothes.component.scss',
+    imports: [ClothesFilterComponent, ClothesFeedComponent, ClothesSortingComponent]
 })
-export default class ClothesComponent {
+export default class ClothesComponent implements OnInit {
+  private route: ActivatedRoute = inject(ActivatedRoute);
 
+  category: WritableSignal<string> = signal('all');
+  pageNumber: WritableSignal<number> = signal(1);
+  sortByPrice: WritableSignal<string> = signal('');
+  sortByName: WritableSignal<string> = signal('');
+  hideOutOfStock: WritableSignal<boolean> = signal(false);
+
+  // constructor() {
+  //   effect(() => {
+      
+  //   })
+  // }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.category.set(params['category']);
+      this.pageNumber.set(params['pageNumber']);
+      this.sortByPrice.set(params['sortByPrice']);
+      this.sortByName.set(params['sortByName']);
+      this.hideOutOfStock.set(params['hideOutOfStock']);
+    
+      console.log(this.category())
+    })
+  }
 }
