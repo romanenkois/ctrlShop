ebd57326880a83fa0ab90f148603991fa514dcea
Illya <romanenkoisnot@gmail.com>
2024-07-05 02:11:01 +0300

    feat: basic clothes feed

---
+++ src/app/widgets/clothes-page/clothes-feed/clothes-feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clothes-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './clothes-feed.component.html',
+  styleUrl: './clothes-feed.component.scss'
+})
+export class ClothesFeedComponent {
+
+}
