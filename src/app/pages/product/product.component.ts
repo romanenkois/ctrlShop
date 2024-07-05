2020e6e35bbad83c87ad9d65313711d1985a3234
Illya <romanenkoisnot@gmail.com>
2024-07-05 16:45:43 +0300

    feat: basic product page

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ShowcaseComponent } from "../../widgets/product-page/showcase/showcase.component";
+import { ReviewsComponent } from "../../widgets/product-page/reviews/reviews.component";
 
 @Component({
-  selector: 'app-product',
-  standalone: true,
-  imports: [],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+    selector: 'app-product',
+    standalone: true,
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.scss',
+    imports: [ShowcaseComponent, ReviewsComponent]
 })
 export default class ProductComponent {
 
