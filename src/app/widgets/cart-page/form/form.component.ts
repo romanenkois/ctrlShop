bb32f9ec434673a81c796fe2e7ca28a1e2107861
Illya <romanenkoisnot@gmail.com>
2024-07-22 18:47:30 +0300

    feat: cart page forms

--- src/app/widgets/cart-page/form/form.component.ts
+++ src/app/widgets/cart-page/form/form.component.ts
@@ -3,11 +3,12 @@ import { FirstStepComponent } from "./ui/first-step/first-step.component";
 import { SecondStepComponent } from "./ui/second-step/second-step.component";
 import { ThirdStepComponent } from "./ui/third-step/third-step.component";
 import { FourthStepComponent } from "./ui/fourth-step/fourth-step.component";
+import { FifthStepComponent } from "./ui/fifth-step/fifth-step.component";
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FirstStepComponent, SecondStepComponent, ThirdStepComponent, FourthStepComponent],
+  imports: [FirstStepComponent, SecondStepComponent, ThirdStepComponent, FourthStepComponent, FifthStepComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
@@ -16,9 +17,10 @@ createOrder() {
 throw new Error('Method not implemented.');
 }
   hideFirstStep = false;
-  hideSecondStep = true;
+  hideSecondStep = false;
   hideThirdStep = true;
   hideFourthStep = true;
+  hideFifthStep = true;
 
   openNextStep() {
     if (this.hideSecondStep) {
@@ -27,10 +29,8 @@ throw new Error('Method not implemented.');
       this.hideThirdStep = false;
     } else if (this.hideFourthStep) {
       this.hideFourthStep = false;
+    } else if (this.hideFifthStep) {
+      this.hideFifthStep = false;
     }
-    // if (!this.hideFirstStep) {
-    //   this.hideSecondStep == false;
-    //   console.log(123)
-    // }
   }
 }
---
+++ src/app/widgets/cart-page/form/ui/fifth-step/fifth-step.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fifth-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './fifth-step.component.html',
+  styleUrl: './fifth-step.component.scss'
+})
+export class FifthStepComponent {
+
+}
