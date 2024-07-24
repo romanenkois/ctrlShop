debdd9ef341e9605d11b1676d96dc3164727bdf7
Illya <romanenkoisnot@gmail.com>
2024-07-24 19:56:49 +0300

    feat: cart page order upload

--- src/app/widgets/cart-page/form/api/upload.service.ts
+++ src/app/widgets/cart-page/form/api/upload.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
+  private http: HttpClient = inject(HttpClient);
 
-  constructor() { }
+  private BASE_URL: string = 'https://ctrl-shop-back.vercel.app';
+  // private BASE_URL: string = 'http://localhost:3000';
+
+  uploadOrder(
+    date: string,
+    userId: string,
+    orderData: any,
+    customerData: any,
+    deliveryData: any,
+    extraData: any
+  ) {
+    const formData: FormData = new FormData();
+
+    formData.append('date', date);
+    formData.append('userId', userId);
+    formData.append('orderData', JSON.stringify(orderData));
+    formData.append('customerData', JSON.stringify(customerData));
+    formData.append('deliveryData', JSON.stringify(deliveryData));
+    formData.append('extraData', JSON.stringify(extraData));
+
+    return this.http.post<any>(this.BASE_URL + '/order', formData);
+  }
 }
--- src/app/widgets/cart-page/form/form.component.ts
+++ src/app/widgets/cart-page/form/form.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, effect, inject, input, OnInit, signal, WritableSignal } from '@angular/core';
 import { FirstStepComponent } from "./ui/first-step/first-step.component";
 import { CartService } from '../../../shared/cart/cart.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UploadService } from './api/upload.service';
 
 @Component({
   selector: 'app-form',
@@ -11,15 +12,22 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
-  private cartService: CartService = inject(CartService);
+export class FormComponent implements OnInit {
   private fb: FormBuilder = inject(FormBuilder);
+  
+  private cartService: CartService = inject(CartService);
+  private uploadService: UploadService = inject(UploadService);  
 
   hideFirstStep = false;
   hideSecondStep = true;
   hideThirdStep = true;
   hideFourthStep = true;
   hideFifthStep = true;
+
+  completedFirstStep: WritableSignal<boolean> = signal(false);
+  completedSecondStep: WritableSignal<boolean> = signal(false);
+  completedThirdStep: WritableSignal<boolean> = signal(false);
+  completedFourthStep: WritableSignal<boolean> = signal(!false);
 
   customerData: FormGroup = this.fb.group({
     inputName: ['', Validators.required],
@@ -47,37 +55,91 @@ export class FormComponent {
   openNextStep(step: number) {
     switch (step) {
       case 2:
-        this.hideSecondStep = false;
+        if (this.completedFirstStep()) {  
+          this.hideSecondStep = false;
+        }
         break;
       case 3:
-        if (this.customerData.valid) {
+        if (this.completedSecondStep()) {
           this.hideThirdStep = false;
         }
         break;
       case 4:
-        if (this.deliveryData.valid) {
+        if (this.completedThirdStep()) {
           this.hideFourthStep = false;
         }
         break;
       case 5:
-        // if (this.paymentData.valid) {
+        if (this.completedFourthStep()) {
           this.hideFifthStep = false;
-        // }
+        }
         break;
     }
   }
 
   createOrder() {
     if (
-    this.customerData.valid
-    && this.deliveryData.valid) {
+    this.completedFirstStep()
+    && this.completedSecondStep()
+    && this.completedThirdStep()
+    && this.completedFourthStep()){
 
-      
-      window.alert('замовлення (не) створено\nдев давн ще не доробив');
-      // this.cartService.clerCart();
-      // window.location.href = '/';
-      
+      // console.log(new Date().toISOString())
+      // console.log('1')
+      // console.log(this.cartService.$cart)
+      // console.log(typeof this.cartService.$cart)
+      // console.log(this.customerData.value)
+      // console.log(typeof this.customerData.value)
+      // console.log(this.deliveryData.value)
+      // console.log(typeof this.deliveryData.value)
+      // console.log(this.extraData.value)
+      // console.log(typeof this.extraData.value)
+
+      this.cartService.$cart.subscribe((cart) => {
+        this.uploadService.uploadOrder(
+          new Date().toISOString(),
+          '1',
+          cart,
+          this.customerData.value,
+          this.deliveryData.value,
+          this.extraData.value ? this.extraData.value : {}
+        ).subscribe(() => {
+          window.alert('замовлення успішно відправлено\nдякуємо!!');
+          this.cartService.clerCart();
+          window.location.href = '/';
+        });
+      });
+
+    } else if (!this.completedFirstStep()) {
+      window.alert('чєл, корзина пуста');
+    } else {
+      window.alert('заповни всі поля попуск');
     }
-    
+  }
+
+  ngOnInit() {
+    this.cartService.$cart.subscribe((cart) => {
+      if (cart.length) {
+        this.completedFirstStep.set(true);
+      } else {
+        this.completedFirstStep.set(false);
+      }
+    })
+
+    this.customerData.valueChanges.subscribe(() => {
+      if (this.customerData.get('inputName') && this.customerData.get('inputEmail')){
+        this.completedSecondStep.set(true);
+      } else {
+        this.completedSecondStep.set(false);
+      }
+    });
+
+    this.deliveryData.valueChanges.subscribe(() => {
+      if (this.deliveryData.get('inputCountry') && this.deliveryData.get('inputCity') && this.deliveryData.get('inputPostOffice')){
+        this.completedThirdStep.set(true);
+      } else {
+        this.completedThirdStep.set(false);
+      }
+    });
   }
 }
