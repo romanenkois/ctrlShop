99cf91fdb7833de597057db0de8bd26d4fad2378
Illya <romanenkoisnot@gmail.com>
2024-09-04 20:36:24 +0300

    feat: review form

--- src/app/widgets/product-page/reviews/reviews.component.ts
+++ src/app/widgets/product-page/reviews/reviews.component.ts
@@ -1,19 +1,29 @@
 import { Component, computed, inject, OnInit } from '@angular/core';
 import { ReviewsService } from './api/reviews.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss'
 })
 export class ReviewsComponent implements OnInit {
+sendReview() {
+throw new Error('Method not implemented.');
+}
   private router: ActivatedRoute = inject(ActivatedRoute);
   private reviewsService: ReviewsService = inject(ReviewsService);
+  private fb: FormBuilder = inject(FormBuilder);
 
   reviews = computed(() => this.reviewsService.productReviews());
+
+  customerReview: FormGroup = this.fb.group({
+    customerReviewText: ['', Validators.required],
+  });
 
   ngOnInit() {
     this.router.url.subscribe(url => {
