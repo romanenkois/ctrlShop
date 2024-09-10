8fc530993d065c23f574f83b7d96733a302d188d
Illya <romanenkoisnot@gmail.com>
2024-09-11 01:34:45 +0300

    feat: product rating

--- src/app/widgets/product-page/reviews/ui/new-review/new-review.component.ts
+++ src/app/widgets/product-page/reviews/ui/new-review/new-review.component.ts
@@ -1,21 +1,22 @@
-import { Component, inject, input, InputSignal } from '@angular/core';
+import { Component, effect, inject, input, InputSignal, signal, WritableSignal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ReviewsService } from '../../api/reviews.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-review',
   standalone: true,
-  imports: [ ReactiveFormsModule ],
+  imports: [ ReactiveFormsModule, CommonModule ],
   templateUrl: './new-review.component.html',
   styleUrl: './new-review.component.scss'
 })
 export class NewReviewComponent {
-  private router: ActivatedRoute = inject(ActivatedRoute);
   private reviewsService: ReviewsService = inject(ReviewsService);
   private fb: FormBuilder = inject(FormBuilder);
 
   productId: InputSignal<string> = input.required();
+  reviewRating: WritableSignal<number> = signal(5);
 
   sendingReview = false;
 
@@ -23,17 +24,21 @@ export class NewReviewComponent {
     customerReviewText: ['', Validators.required],
   });
 
+  setReviewRating(rating: number) {
+    this.reviewRating.set(rating);
+  }
+
   sendReview() {
     if (this.customerReview.valid && !this.sendingReview) {
       this.sendingReview = true;
 
       this.reviewsService.sendNewReview(
         new Date().toISOString(),
-        '1',
+        '__user_id__',
         'джоу доу',
         this.productId(),
         this.customerReview.get('customerReviewText')?.value,
-        5,
+        this.reviewRating(),
       ).subscribe((res: any) => {
         window.alert('ваш відгук успрішно надіслано!\nвін зв\'явиться на сайті щойно пройде модеріцію')
         this.sendingReview = false;
