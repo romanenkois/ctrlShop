e9667c540c9df91e000e5f6484dffea3b5cf121d
Illya <romanenkoisnot@gmail.com>
2024-10-01 19:39:41 +0300

    feat: lazy-load
    lazy-load of items on clothes page

--- src/app/widgets/clothes-page/clothes-feed/ui/clothes-card/clothes-card.component.ts
+++ src/app/widgets/clothes-page/clothes-feed/ui/clothes-card/clothes-card.component.ts
@@ -1,18 +1,17 @@
-import { ChangeDetectorRef, Component, ElementRef, inject, input, Input, InputSignal, Renderer2 } from '@angular/core';
+import { Component, inject, input, InputSignal } from '@angular/core';
 import { CartService } from '../../../../../shared/cart/cart.service';
 import { FavoritesService } from '../../../../../shared/favorites/favorites.service';
 import { TranslateTypePipe } from "../../../../../shared/pipes/translate-type.pipe";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clothes-card',
   standalone: true,
-  imports: [TranslateTypePipe],
+  imports: [TranslateTypePipe, CommonModule],
   templateUrl: './clothes-card.component.html',
   styleUrl: './clothes-card.component.scss'
 })
 export class ClothesCardComponent {
-  private renderer: Renderer2 = inject(Renderer2);
-
   private cartService: CartService = inject(CartService);
   private favoritesService: FavoritesService = inject(FavoritesService); 
 
@@ -21,22 +20,14 @@ export class ClothesCardComponent {
   addToCart(productId: any, button: HTMLElement) {
     this.cartService.addToCart(productId);
 
-    // Apply slyles to the button after action has been indeed done
-    this.renderer.setStyle(button, 'transition', 'background 0.5s ease-out');
-    this.renderer.setStyle(button, 'background', 'var(--yellow-color)');
-    setTimeout(() => {
-      this.renderer.setStyle(button, 'background', '');
-    }, 1000);
+    button.classList.add('click-animation');
+    setTimeout(() => button.classList.remove('click-animation'), 1000);
   }
 
   addToFavorites(productId: any, button: HTMLElement) {
     this.favoritesService.addToFavorites(productId);
 
-    // Apply slyles to the button after action has been indeed done
-    this.renderer.setStyle(button, 'transition', 'background 0.5s ease-out');
-    this.renderer.setStyle(button, 'background', 'var(--yellow-color)');
-    setTimeout(() => {
-      this.renderer.setStyle(button, 'background', '');
-    }, 1000);
+    button.classList.add('click-animation');
+    setTimeout(() => button.classList.remove('click-animation'), 1000);
   }
 }
