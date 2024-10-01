7fa2bf0349035ad38955fc04abf8bf2c914dbf5b
Illya <romanenkoisnot@gmail.com>
2024-10-01 20:54:38 +0300

    feat: loading styles
    loading styles for clothes page

--- src/app/widgets/clothes-page/clothes-feed/clothes-feed.component.ts
+++ src/app/widgets/clothes-page/clothes-feed/clothes-feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, input, InputSignal, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, effect, inject, input, InputSignal, signal, WritableSignal } from '@angular/core';
 import { clothesService } from './api/clothes.service';
 import { ClothesCardComponent } from "./ui/clothes-card/clothes-card.component";
 
@@ -9,7 +9,7 @@ import { ClothesCardComponent } from "./ui/clothes-card/clothes-card.component";
   templateUrl: './clothes-feed.component.html',
   styleUrl: './clothes-feed.component.scss'
 })
-export class ClothesFeedComponent implements OnInit {
+export class ClothesFeedComponent {
   private clothesService: clothesService = inject(clothesService);
 
   category: InputSignal<string> = input.required();
@@ -18,7 +18,7 @@ export class ClothesFeedComponent implements OnInit {
   sortByPrice: InputSignal<string> = input.required();
   hideOutOfStock: InputSignal<string> = input.required();
 
-  clothesList: WritableSignal<any> = signal(null);
+  clothesList: WritableSignal<any> = signal([{}, {}, {}]);
   itemsCount: WritableSignal<number> = signal(0);
 
   constructor() {
@@ -27,69 +27,8 @@ export class ClothesFeedComponent implements OnInit {
         response => {
           this.clothesList.set(response['result']);
           this.itemsCount.set(response['itemsCount']);
-          // console.log(response);
         }
       )
     })
-  }
-
-  ngOnInit() {
-
-    // testing
-    // this.clothesList.set(
-    
-    //   [
-    //       {
-    //         "_id": "668c09a5c0e1256172e557a9",
-    //         "name": "шарффффффф2",
-    //         "price": "599",
-    //         "description": "мяуууу",
-    //         "image": [
-    //           "https://ctrl-shop-back.vercel.app/image/668c09a3c0e1256172e557a8"
-    //         ],
-    //         "category": "scarf"
-    //       },
-    //       {
-    //         "_id": "668eafc365ca4d8aa4e1ae4c",
-    //         "name": "тянкошарфф",
-    //         "category": "scarf",
-    //         "price": "699",
-    //         "description": "міяуу мяу мяу",
-    //         "image": [
-    //           "https://ctrl-shop-back.vercel.app/image/668eafc365ca4d8aa4e1ae4b"
-    //         ]
-    //       },
-    //       {
-    //         "_id": "668eafea65ca4d8aa4e1ae4e",
-    //         "name": "штани труби",
-    //         "category": "pants",
-    //         "price": "999",
-    //         "description": "типу такий, не такий",
-    //         "image": [
-    //           "https://ctrl-shop-back.vercel.app/image/668eafe965ca4d8aa4e1ae4d"
-    //         ]
-    //       },
-    //       {
-    //         "_id": "668eb00565ca4d8aa4e1ae50",
-    //         "name": "зіпка 20221",
-    //         "category": "zipka",
-    //         "price": "1199",
-    //         "description": "вауууууууууууууууууууууу",
-    //         "image": [
-    //           "https://ctrl-shop-back.vercel.app/image/668eb00565ca4d8aa4e1ae4f"
-    //         ]
-    //       },
-    //       {
-    //         "_id": "668eb41065ca4d8aa4e1ae52",
-    //         "name": "футболка-мультики",
-    //         "category": "tshirt",
-    //         "price": "499",
-    //         "description": "тудааа попусків",
-    //         "image": [
-    //           "https://ctrl-shop-back.vercel.app/image/668eb40f65ca4d8aa4e1ae51"
-    //         ]
-    //       }
-    //     ]
-    // )
   }
 }
