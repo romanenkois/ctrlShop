9e81204a3c1cb34a861251882e8e0d623743f287
Illya <romanenkoisnot@gmail.com>
2024-10-02 16:54:44 +0300

    feat: basic login logic

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { AuthorizationService } from '../../shared/authorization/authorization.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export default class UserComponent {
+export default class UserComponent implements OnInit {
+  private router = inject(Router);
   
+  authService: AuthorizationService = inject(AuthorizationService);
+
+  userData = computed(() => this.authService.userData());
+
+  signOut() {
+    this.authService.signOut();
+  }
+  
+  ngOnInit() {
+    console.log('User component initialized');
+    console.log(this.userData());
+    if (!this.userData().name) {
+      this.router.navigate(['/login']);
+    }
+  }
 }
--- src/app/shared/authorization/authorization.service.ts
+++ src/app/shared/authorization/authorization.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@angular/core';
+declare var google: any;
+
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
+  private router = inject(Router);
+  
+  public userData: WritableSignal<any> = signal(null);
 
-  constructor() { }
+  private decodeToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+
+  handleGoogleLogin(res: any) {
+    if (res) {
+      const payload = this.decodeToken(res.credential);
+      sessionStorage.setItem('loggedInUser', JSON.stringify(payload));
+      this.loadUserData();
+      this.router.navigate(['/user']);
+    }  
+  }
+
+  signOut() {
+    sessionStorage.removeItem('loggedInUser');
+    this.userData.set(null);
+    google.accounts.id.disableAutoSelect();
+    this.router.navigate(['/home']);
+  }
+
+  loadUserData() {
+    let newUserData: any = {}
+    try {
+      const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser')!);
+      if (loggedInUser) {
+        newUserData.name = loggedInUser.name;
+        newUserData.picture = loggedInUser.picture;
+        newUserData.email = loggedInUser.email;
+      }
+    } catch (error) {
+      console.error('Could`t parse user authorization data');
+    }
+    this.userData.set(newUserData);
+  }
+
+  constructor() {
+    this.loadUserData();
+  }
 }
