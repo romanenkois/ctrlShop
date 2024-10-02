193b3eb524968132ba366e5144a3b26aee25bde0
Illya <romanenkoisnot@gmail.com>
2024-10-02 23:00:35 +0300

    fix: api

--- src/app/widgets/clothes-page/clothes-feed/api/clothes.service.ts
+++ src/app/widgets/clothes-page/clothes-feed/api/clothes.service.ts
@@ -7,8 +7,8 @@ import { inject, Injectable } from '@angular/core';
 export class clothesService {
   private http: HttpClient = inject(HttpClient);
 
-  // private BASE_URL: string = 'https://ctrl-shop-back.vercel.app';
-  private BASE_URL: string = 'http://localhost:3000';
+  private BASE_URL: string = 'https://ctrl-shop-back.vercel.app';
+  // private BASE_URL: string = 'http://localhost:3000';
 
   getClothesData(
     category: string,
