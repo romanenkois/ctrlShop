7fa476a274b61bb25727180dfc7aa22d55047671
Illya <romanenkoisnot@gmail.com>
2024-11-14 20:10:13 +0200

    styles: clothes feed

--- src/app/widgets/clothes-page/clothes-feed/clothes-feed.component.ts
+++ src/app/widgets/clothes-page/clothes-feed/clothes-feed.component.ts
@@ -1,11 +1,12 @@
 import { Component, effect, inject, input, InputSignal, signal, WritableSignal } from '@angular/core';
 import { clothesService } from './api/clothes.service';
 import { ClothesCardComponent } from "./ui/clothes-card/clothes-card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clothes-feed',
   standalone: true,
-  imports: [ClothesCardComponent],
+  imports: [ClothesCardComponent, CommonModule],
   templateUrl: './clothes-feed.component.html',
   styleUrl: './clothes-feed.component.scss'
 })
@@ -34,7 +35,10 @@ export class ClothesFeedComponent {
         sorting = 'price-desc';
       }
 
-      this.clothesList.set([{}, {}]);
+      // before fetch, we do this for better loading view
+      this.clothesList.set([{}, {}, {}]);
+      this.itemsCount.set(1);
+
 
       this.clothesService.getClothesData(this.category(), this.pageNumber(), sorting).subscribe(
         response => {
